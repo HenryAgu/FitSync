a844eb9e5328a7f8256fc9d30877658ecaa96414
Henry <aguhenrychuks@gmail.com>
2023-06-10 17:34:26 +0100

    feat: team name added

--- src/Landing/components/Team/Team.jsx
+++ src/Landing/components/Team/Team.jsx
@@ -12,9 +12,24 @@ const Team = () => {
                 <p>Get to know the passionate individuals behind our success.</p>
             </div>
             <div className="team_info">
-                <div className="team_card first_card"></div>
-                <div className="team_card second_card"></div>
-                <div className="team_card third_card"></div>
+                <div className="team_card first_card">
+                  <div className="member_name">
+                    <h3>ADAH JONATHAN</h3>
+                    <p>CEO, FitSync</p>
+                  </div>
+                </div>
+                <div className="team_card second_card">
+                  <div className="member_name">
+                    <h3>LINDA NAOMI</h3>
+                    <p>COO, FitSync</p>
+                  </div>
+                </div>
+                <div className="team_card third_card">
+                  <div className="member_name">
+                    <h3>MATT WALSH</h3>
+                    <p>Lead Gym Instructor, FitSync</p>
+                  </div>
+                </div>
             </div>
         </div>
     </main>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,76 @@
 // import React from 'react'
 
 // stylesheet
+import "../../styles/Footer.scss";
+
+// Nav link
+import { NavLink } from "react-router-dom";
+
+// Images
+import FooterLogo from "../Footer/images/FooterLogo.svg";
+import Twitter from "../Footer/images/Twitter.svg";
+import Instagram from "../Footer/images/Instagram.svg";
+import Youtube from "../Footer/images/YouTube.svg";
+import LinkedIn from "../Footer/images/LinkedIn.svg";
+import Facebook from "../Footer/images/Facebook.svg";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <div className="footer_left">
+        <img src={FooterLogo} alt="Logo" />
+        <h4>
+          At FitSync, we are dedicated to transforming lives through fitness
+        </h4>
+        <div className="left_social_icons">
+          <NavLink to="">
+            <img src={Twitter} alt="Twitter" />
+          </NavLink>
+          <img src={Instagram} alt="Instagram" />
+          <img src={Youtube} alt="Youtube" />
+          <img src={LinkedIn} alt="LinkedIn" />
+          <img src={Facebook} alt="Facebook" />
+        </div>
+      </div>
+      <div className="footer_middle">
+        <div className="inner_middle">
+          <NavLink to="" style={{ textDecoration: "none" }}>
+            <p>Programs</p>
+          </NavLink>
+          <NavLink to="" style={{ textDecoration: "none" }}>
+            <p>Testimonials</p>
+          </NavLink>
+          <NavLink to="" style={{ textDecoration: "none" }}>
+            <p>Blog</p>
+          </NavLink>
+          <NavLink to="" style={{ textDecoration: "none" }}>
+            <p>Store</p>
+          </NavLink>
+        </div>
+        <div className="inner_middle">
+          <NavLink to="" style={{ textDecoration: "none" }}>
+            <p>Membership</p>
+          </NavLink>
+          <NavLink to="" style={{ textDecoration: "none" }}>
+            <p>Community</p>
+          </NavLink>
+          <NavLink to="" style={{ textDecoration: "none" }}>
+            <p>Contact Us</p>
+          </NavLink>
+          <NavLink to="" style={{ textDecoration: "none" }}>
+            <p>FAQs</p>
+          </NavLink>
+        </div>
+      </div>
+      <div className="footer_right">
+        <h3>JOIN US TODAY</h3>
+        <p>Join us today and embark on a transformative fitness journey.</p>
+        <NavLink to="">
+          <button>Join Now</button>
+        </NavLink>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
